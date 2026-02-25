b5542e8a9779f19bc2433abc97456572e4d98577
Anicett Jeff Takayama <137365454+Yami2Danchou@users.noreply.github.com>
2026-02-26 02:07:07 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs', 'cloudinary'],
-  },
-  // Add this to handle CSS imports
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Don't resolve 'fs' module on the client to prevent this error
-      config.resolve.fallback = {
-        fs: false,
-        net: false,
-        tls: false,
-      }
-    }
-    return config
-  },
-}
+  /* config options here */
+};
 
-module.exports = nextConfig
+export default nextConfig;
